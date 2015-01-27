d675d6170179ee621879340db5f4cc9e26325d16
tpmanc <tpxtrime@mail.ru>
2015-01-27 21:07:35 +0400

    configure db

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=football',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
