07ec1b23485679bfa863b7d01467563e7d6bb64d
tpmanc <tpxtrime@mail.ru>
2015-01-29 00:28:10 +0400

    include animate js

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -19,8 +19,10 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'css/animate.css',
     ];
     public $js = [
+		'js/animate.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
