626c499f9030b5192f0c5a506f753d051112025e
tpmanc <tpxtrime@mail.ru>
2015-01-30 19:48:16 +0400

    fix match controller

--- controllers/MatchController.php
+++ controllers/MatchController.php
@@ -8,6 +8,7 @@ use app\models\MatchesSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
 
 /**
  * MatchController implements the CRUD actions for Matches model.
@@ -17,6 +18,15 @@ class MatchController extends Controller
     public function behaviors()
     {
         return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
