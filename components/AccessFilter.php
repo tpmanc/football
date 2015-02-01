a765de25bf37bbf61f917518c0cff1acde7f2cd2
tpmanc <tpxtrime@mail.ru>
2015-02-01 12:40:40 +0400

    make access filer, based on user->isAdmin

---
+++ components/AccessFilter.php
@@ -0,0 +1,24 @@
+<?php 
+namespace app\components;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\base\ActionFilter;
+use yii\web\ForbiddenHttpException;
+
+class AccessFilter extends AccessControl
+{
+	public function init()
+	{
+		parent::init();
+	}
+	
+	public function beforeAction($action)
+	{
+		if( !\Yii::$app->user->isGuest && \Yii::$app->user->identity->isAdmin ){
+			return true;
+		}
+		throw new ForbiddenHttpException('You are not allowed to perform this action.');
+		return false;
+	}
+}
